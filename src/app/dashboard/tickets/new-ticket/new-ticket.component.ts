668d4c26a880818cbf16449a7b7bae3a23327f8e
Yash dixit <yash9369d@gmail.com>
2024-10-12 19:18:54 +0530

    using-signal-component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -16,11 +16,14 @@ export class NewTicketComponent {
   // }
 
 
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // @ViewChildren(ButtonComponent) ---> for multiple button
+  // --> 17.3 version available , Signal use case -->
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); 
 
   onSubmit(title: string, ticketText: string) {
     console.log(title)
     console.log(ticketText)
-    this.form?.nativeElement.reset();
+    this.form()?.nativeElement.reset();
   }
 }
