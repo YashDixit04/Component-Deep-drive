573cb92733e4e1f3d90286dacb076a759ee9cf91
Yash dixit <yash9369d@gmail.com>
2024-10-13 11:51:53 +0530

    Signal-use-in-sever.ts-and-effect-useCase

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,7 +8,9 @@ import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core'
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = "offline";
+  // currentStatus: 'online' | 'offline' | 'unknown' = "offline";
+  // Signal method --->>>
+  currentStatus = signal<'online' | 'offline' | 'unknown'>("offline");
   private destroyRef = inject(DestroyRef);
   // private interval?: ReturnType<typeof setInterval>;
 
@@ -26,22 +28,36 @@ export class ServerStatusComponent implements OnInit {
   //     }
   //   }, 5000)
   // }
+  constructor(){
+    effect(()=>{
+      console.log(this.currentStatus())
+    })
+  }
 
   ngOnInit() {
     console.log('ON INIT')
-   const interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
+      // if (rnd < 0.5) {
+      //   this.currentStatus = 'online';
+      // } else if (rnd < 0.9) {
+      //   this.currentStatus = 'offline';
+      // }
+      // else {
+      //   this.currentStatus = 'unknown';
+      // }
 
+      // signal ---->>>
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('offline');
       }
       else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000)
-    this.destroyRef.onDestroy(()=>{
+    this.destroyRef.onDestroy(() => {
       clearInterval(interval)
     })
   }
